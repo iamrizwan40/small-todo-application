8ea55a0e0979faef0f9e49531b891ad87583997a
Mohd Rizwan <morizwanansar786@gmail.com>
2019-02-14 21:18:57 -0800

    small to-do app

---
+++ main.js
@@ -0,0 +1,32 @@
+
+let btn = document.querySelector('button');
+let input = document.querySelector('input[type=\'text\']');
+btn.addEventListener('click', addTask);
+let numberOfItems = 0;
+let delBtn;
+function addTask(e){
+	e.preventDefault();
+	let ul = document.querySelector('ul');
+	let li = document.createElement('li');
+	let textNode =  document.createTextNode(input.value);
+	delBtn = document.createElement('button');
+	let btnValue = document.createTextNode('Delete Task');
+	delBtn.append(btnValue);
+	li.append(input.value);
+	li.append(delBtn);
+	ul.append(li);
+	input.value = '';
+	numberOfItems++;
+	document.querySelector('.numOfItems').innerHTML = numberOfItems;
+	delBtn.classList = 'btn btn-danger';
+	
+	let listBtn = document.querySelectorAll('button.btn');
+
+	listBtn.forEach(btn => {
+		btn.onclick = function(){
+			btn.parentNode.classList = 'hide';
+			numberOfItems--;
+			document.querySelector('.numOfItems').innerHTML = numberOfItems;
+		}
+	})
+}
